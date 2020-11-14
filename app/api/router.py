7ab8838f5b81061af8a2c8ac5891a6298bb1b356
jozuo <jozuo.dev@gmail.com>
2020-11-14 14:34:41 +0900

    APIキー認証のOpenAPI設定追加

--- app/api/router.py
+++ app/api/router.py
@@ -1,16 +1,32 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Security
+from fastapi.security.api_key import APIKeyHeader
 
 from app.api.controllers import dog_controller, image_controller, owner_controller
+
+api_key_authorization = Security(
+    APIKeyHeader(
+        name="x-api-key",
+        scheme_name="authorization",
+    )
+)
+
 
 router = APIRouter()
 router.include_router(
     owner_controller.router,
     prefix="/owners",
     tags=["owner"],
+    dependencies=[api_key_authorization],
 )
 router.include_router(
-    dog_controller.router, prefix="/owners/{owner_id}/dogs", tags=["dogs"]
+    dog_controller.router,
+    prefix="/owners/{owner_id}/dogs",
+    tags=["dogs"],
+    dependencies=[api_key_authorization],
 )
 router.include_router(
-    image_controller.router, prefix="/owners/{owner_id}/images", tags=["images"]
+    image_controller.router,
+    prefix="/owners/{owner_id}/images",
+    tags=["images"],
+    dependencies=[api_key_authorization],
 )
