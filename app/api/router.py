758367603ca492dcc4a63c9ef8b358fc87a1975b
jozuo <jozuo.dev@gmail.com>
2021-05-16 17:13:48 +0900

    feat: APIキー認証からIAM認証に変更

--- app/api/router.py
+++ app/api/router.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, Security
-from fastapi.security.api_key import APIKeyHeader
+from fastapi import APIRouter
 
 from app.api.controllers import (
     dog_controller,
@@ -9,42 +8,29 @@ from app.api.controllers import (
     task_controller,
 )
 
-api_key_authorization = Security(
-    APIKeyHeader(
-        name="x-api-key",
-        scheme_name="authorization",
-    )
-)
-
-
 router = APIRouter()
 router.include_router(
     owner_controller.router,
     prefix="/owners",
     tags=["owner"],
-    dependencies=[api_key_authorization],
 )
 router.include_router(
     dog_controller.router,
     prefix="/owners/{owner_id}/dogs",
     tags=["dogs"],
-    dependencies=[api_key_authorization],
 )
 router.include_router(
     image_controller.router,
     prefix="/owners/{owner_id}/images",
     tags=["images"],
-    dependencies=[api_key_authorization],
 )
 router.include_router(
     task_controller.router,
     prefix="/owners/{owner_id}/tasks",
     tags=["tasks"],
-    dependencies=[api_key_authorization],
 )
 router.include_router(
     event_controller.router,
     prefix="/owners/{owner_id}/events",
     tags=["events"],
-    dependencies=[api_key_authorization],
 )
